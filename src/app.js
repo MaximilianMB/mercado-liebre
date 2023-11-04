c5ef254df93e1c7c1fe4a0f52356740865ec6d13
MaximilianMB <maxi_1204@hotmail.com>
2023-11-04 14:26:15 -0300

    mitad de clase 21

--- src/app.js
+++ src/app.js
@@ -11,3 +11,11 @@ app.listen(3000, function(){
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
+
+app.get("/register", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
